d27706aeb07b05117189d46d378cad56ef44634d
aminnademi <mgamer7557@gmail.com>
2024-07-25 15:58:59 +0330

    added CofirmInfo ViewModel with some changes in UI

---
+++ src/Asp/ViewModels/Reserve/ConfirmInfoViewModel.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace Application.ViewModels.Reserve
+{
+  public class ConfirmInfoViewModel
+  {
+    public string TripCode { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Numberphone { get; set; }
+    public string Nacode { get; set; }
+  }
+}
